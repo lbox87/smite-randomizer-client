5662bee58453e4c953614ae2bd26d12bbb568eff
Andrew Kolb <akolb87@gmail.com>
2019-03-19 19:47:17 -0500

    topnav init

---
+++ src/components/top-nav.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function TopNav() {
+    return (
+        <div className="links">
+            <a href="">Register</a>
+            <a href="">Continue without log in.</a>
+        </div>
+    );
+}
