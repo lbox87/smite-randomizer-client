7157796db11d24ba53a29db7505cb7bcf84e9284
Andrew Kolb <akolb87@gmail.com>
2019-04-07 19:42:56 -0500

    god result page stateful

--- src/components/god-result-page.js
+++ src/components/god-result-page.js
@@ -4,14 +4,32 @@ import GodResult from './god-result';
 import ItemResultsPage from './item-results-page';
 // import './landing-page.css';
 
-export default function GodResultPage(props) {
+export default class GodResultPage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+        }
+    }
+render() {
     return (
         <div className=''>
-            <h2>You Randomed <span className="god-name-result">{props.god}</span></h2>
-            <img src={props.image} alt={props.god} className="god-image" />
+            <h2>You Randomed <span className="god-name-result">{this.props.god}</span></h2>
+            <img src={this.props.image} alt={this.props.god} className="god-image" />
 
             <ItemResultsPage />
             
         </div>
     );
 }
+}
+// export default function GodResultPage(props) {
+//     return (
+//         <div className=''>
+//             <h2>You Randomed <span className="god-name-result">{props.god}</span></h2>
+//             <img src={props.image} alt={props.god} className="god-image" />
+
+//             <ItemResultsPage />
+            
+//         </div>
+//     );
+// }
