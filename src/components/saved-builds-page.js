8f3f8914c49379711119dd313d503aa693ecba0f
Andrew Kolb <akolb87@gmail.com>
2019-04-26 20:41:14 -0500

    cleanup

--- src/components/saved-builds-page.js
+++ src/components/saved-builds-page.js
@@ -1,21 +1,5 @@
 // import React from 'react';
 import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
-
-// import SavedBuilds from './saved-builds';
-// // import './landing-page.css';
-
-// export default function SavedBuildsPage() {
-//     return (
-//         <div className='container'>
-//             <h2>Your Saved Builds</h2>
-//             <p>Click Edit Build to re-roll items in your build.</p>
-//             <p>Click Delete Build to remove it from your list.</p>
-//             <button type="button" className="new-random"> <Link to="/results" className='link'>Randomize a New God</Link></button>
-//             <SavedBuilds />
-//         </div>
-//     );
-// }
-
 import React from 'react';
 import {connect} from 'react-redux';
 import requiresLogin from './requires-login';
@@ -28,88 +12,35 @@ export class SavedBuildsPage extends React.Component {
     }
 
     render() {
-        console.log(this.props.protectedData)
+        console.log(this.props.protectedData[0])
         const myBuilds = this.props.protectedData.map((build, index) =>
         <li key={index}>
             <p>{build.god}</p>
-            <img src={build.image} />
-            <img src={build.image1} />
-            <img src={build.image2} />
-            <img src={build.image3} />
-            <img src={build.image4} />
-            <img src={build.image5} />
-            <img src={build.image6} />
+            <img src={build.image} class="god-image"/>
+            <img src={build.image1} class="item"/>
+            <img src={build.image2} class="item"/>
+            <img src={build.image3} class="item"/>
+            <img src={build.image4} class="item"/>
+            <img src={build.image5} class="item"/>
+            <img src={build.image6} class="item"/>
         </li>
     );
-    // let thisBuild = {};
-    // let myBuilds = []
-    // let returnedBuilds = this.props.protectedData;
-    // for(let i = 0; i < returnedBuilds.length; i++){
-    //     console.log(returnedBuilds[i])
-    //     let myBuilds = Object.keys(returnedBuilds[i]).map(key => 
-    //         <SavedBuilds   />
-            // {...key} {...returnedBuilds[i][key]}
-            // <li value={key}>{tifs[key]}</li>
-        // )
-        // myBuilds.push(<img src={returnedBuilds[i].image1} alt={returnedBuilds[i].god} className="god-image"/>)
-        // myBuilds.push(<img src={returnedBuilds[i].image1} alt={returnedBuilds[i].item1} id="item-1"/>)
-        // myBuilds.push(<img src={returnedBuilds[i].image2} alt={returnedBuilds[i].item2} id="item-2"/>)
-        // myBuilds.push(<img src={returnedBuilds[i].image3} alt={returnedBuilds[i].item3} id="item-3"/>)
-        // myBuilds.push(<img src={returnedBuilds[i].image4} alt={returnedBuilds[i].item4} id="item-4"/>)
-        // myBuilds.push(<img src={returnedBuilds[i].image5} alt={returnedBuilds[i].item5} id="item-5"/>)
-        // myBuilds.push(<img src={returnedBuilds[i].image6} alt={returnedBuilds[i].item6} id="item-6"/>)
-                // Object.keys(returnedBuilds[i]).map(
-            // key => 
-                // <img src={returnedBuilds[i].image1} alt={returnedBuilds[i].god} className="god-image"/>
-                // <img src={returnedBuilds[i].image1} alt={returnedBuilds[i].item1} id="item-1"/>
-                // <img src={returnedBuilds[i].image2} alt={returnedBuilds[i].item2} id="item-2"/>
-                // <img src={returnedBuilds[i].image3} alt={returnedBuilds[i].item3} id="item-3"/>
-                // <img src={returnedBuilds[i].image4} alt={returnedBuilds[i].item4} id="item-4"/>
-                // <img src={returnedBuilds[i].image5} alt={returnedBuilds[i].item5} id="item-5"/>
-                // <img src={returnedBuilds[i].image6} alt={returnedBuilds[i].item6} id="item-6"/>
-                // <button type="button" className="edit-build-button"> Edit Build </button>
-                // <button type="button" className="delete-build-button"> Delete Build </button>
-            
-        // )
-    // };
-    // console.log (myBuilds)
-
-    var tifs = {1: 'Joe', 2: 'Jane'};
-
-    // var tifOptions = Object.keys(tifs).forEach(function(key) {
-    //     return <option value={key}>{tifs[key]}</option>
-    // });
-
-    const tifOptions = Object.keys(tifs).map(key => 
-        <li value={key}>{tifs[key]}</li>
-    )
-
-    console.log(tifOptions)
-// for each object indexed in the response array
-// define a variable to equal that indexed object
-// make the key:values the props of a Build component as in the Cards example
-// OR SHORT-TERM map the key:values into a variable as in the tifs example
-    
-
-    console.log(this.props.protectedData)
-
-
+    console.log(myBuilds);
+    if (myBuilds.length === 0) {
+        return (
+            <p>You do not have any saved builds yet! Get started<Link to="/randomize" className='link'>here!</Link></p>
+        )
+    }
         return (
             <div className="dashboard">
                 <div className="dashboard-username">
-                    Username: {this.props.username}
+                    You are logged in as <span id="user">{this.props.currentUser.username}</span>
                 </div>
-                <div className="dashboard-name">Name: {this.props.name}</div>
                 <div className="dashboard-protected-data">
-                    {/* Protected data: {this.props.protectedData} */}
                     <h2>Your Saved Builds</h2>
-                    <p>Click Edit Build to re-roll items in your build.</p>
+                    {/* <p>Click Edit Build to re-roll items in your build.</p> */}
                     <p>Click Delete Build to remove it from your list.</p>
-                    <button type="button" className="new-random"> 
-                    <Link to="/results" className='link'>Randomize a New God</Link></button>
-                    {/* <SavedBuilds /> */}
                     <ul>
-                        {/* {tifOptions} */}
                         {myBuilds}
                     </ul>
                 </div>
