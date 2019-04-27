bc376fc64635d9f88145898d29b139a59332fcc9
Andrew Kolb <akolb87@gmail.com>
2019-04-26 19:51:51 -0500

    header viewchange with login

--- src/components/header.js
+++ src/components/header.js
@@ -1,38 +1,43 @@
 import React from 'react';
-import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
-
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import {connect} from 'react-redux';
 import './header.css';
 
-export default function Header() {
+export function Header(props) {
+    if (props.loggedIn) {
+        return (
+<header className='container'>
+            <h1>SMITE God Randomizer</h1>
+            <nav>
+                <div className='links-top-wrapper'>
+                    <ul className="ul-top">
+                        <li><Link to="/randomize" className='link'>Roll a New God</Link></li>
+                        <li><Link to="/mybuilds" className='link'>My Saved Builds</Link></li>
+                    </ul>
+                </div>
+            </nav>
+            {/* <TopNav /> */}
+        </header>
+        )
+    }
     return (
         <header className='container'>
             <h1>SMITE God Randomizer</h1>
             <nav>
-            <div className='links-top-wrapper'>
-            <ul className="ul-top">
-            <li>
-                    {/* <a href=""
-                        className=""
-                        aria-label="Sign in"
-                    //   onClick={() => props.onSignIn()}
-                    >
-                        Sign in
-                    </a> */}
-                    <Link to="/" className='link'>Sign in</Link>
-                </li>
-                <li>
-                    <Link to="/registration" className='link'>Register</Link>
-                </li>
-                <li>
-                    <Link to="/randomize" className='link'>Continue without log in</Link>
-                </li>
-                <li>
-                    <Link to="/mybuilds" className='link'>My Saved Builds</Link>
-                </li>
-            </ul>
-            </div>
-        </nav>
+                <div className='links-top-wrapper'>
+                    <ul className="ul-top">
+                        <li><Link to="/" className='link'>Sign in</Link></li>
+                        <li><Link to="/registration" className='link'>Register</Link></li>
+                        <li><Link to="/randomize" className='link'>Continue without log in</Link></li>
+                    </ul>
+                </div>
+            </nav>
             {/* <TopNav /> */}
         </header>
     );
 }
+
+const mapStateToProps = state => ({
+    loggedIn: state.auth.currentUser !== null
+});
+export default connect(mapStateToProps)(Header);
