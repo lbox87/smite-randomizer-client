87339a602ae471bbeba37c6e29dcd35147396d1b
Andrew Kolb <akolb87@gmail.com>
2019-04-26 20:03:18 -0500

    hide build when no god

--- src/components/randomize-god-form.js
+++ src/components/randomize-god-form.js
@@ -11,7 +11,6 @@ export class RandomizeGodForm extends React.PureComponent {
   constructor(props) {
     super(props);
     this.state = {
-
       god: "",
       image: "",
       class: ""
@@ -20,10 +19,6 @@ export class RandomizeGodForm extends React.PureComponent {
 
 toggleChangeGod = god => () => {
   this.props.dispatch(toggleGod(god))
-  // this.props.dispatch({
-  //   type: TOGGLE_GOD,
-  //   payload: name
-  // })
 }
 
   onSubmit = (event) => {
@@ -36,19 +31,16 @@ toggleChangeGod = god => () => {
       Warrior: this.props.Warrior
     }
     console.log(JSON.stringify(classFilters));
-
     fetch(API_BASE_URL + 'random3', {
       method: "POST",
       body: JSON.stringify(classFilters),
       headers: { "Content-Type": "application/json" },
     })
-
     .then(response => {
     if (response.ok) {
       return response.json();
     }
     })
-    
     .then(response => {
         console.log(response.gods)
         this.setState( {god: response.gods.name})
@@ -58,57 +50,51 @@ toggleChangeGod = god => () => {
   }
   
   render() {
-    console.log(this.props)
+    const form = [ <form className="randomizer-form" onSubmit={this.onSubmit}>
+    <label htmlFor="assassin-check">Include Assassins</label>
+    <div className="form-input">
+      <input type="checkbox" name="assassin-check" id="assassin-check"
+        checked={this.props.Assassin} onChange={this.toggleChangeGod("Assassin")} />
+    </div>
+
+    <label htmlFor="guardian-check">Include Guardians</label>
+    <div className="form-input">
+      <input type="checkbox" name="guardian-check" id="guardian-check"
+        checked={this.props.Guardian} onChange={this.toggleChangeGod("Guardian")} />
+    </div>
+
+    <label htmlFor="hunter-check">Include Hunters</label>
+    <div className="form-input">
+      <input type="checkbox" name="hunter-check" id="hunter-check"
+        checked={this.props.Hunter} onChange={this.toggleChangeGod("Hunter")} />
+    </div>
+
+    <label htmlFor="mage-check">Include Mages</label>
+    <div className="form-input">
+      <input type="checkbox" name="mage-check" id="mage-check"
+        checked={this.props.Mage} onChange={this.toggleChangeGod("Mage")} />
+    </div>
+
+    <label htmlFor="warrior-check">Include Warriors</label>
+    <div className="form-input">
+      <input type="checkbox" name="warrior-check" id="warrior-check"
+        checked={this.props.Warrior} onChange={this.toggleChangeGod("Warrior")} />
+    </div>
+
+    <button className="randomize-god-button" >Random God!</button>
+  </form>]
+  if (this.state.god === "") {
+    return (
+      <div>{form}</div>
+    )
+  }
     return (
       <div>
-        <form className="randomizer-form" onSubmit={this.onSubmit}>
-          <label htmlFor="assassin-check">Include Assassins</label>
-          <div className="form-input">
-            <input type="checkbox" name="assassin-check" id="assassin-check"
-              checked={this.props.Assassin} onChange={this.toggleChangeGod("Assassin")} />
-          </div>
-
-          <label htmlFor="guardian-check">Include Guardians</label>
-          <div className="form-input">
-            <input type="checkbox" name="guardian-check" id="guardian-check"
-              checked={this.props.Guardian} onChange={this.toggleChangeGod("Guardian")} />
-          </div>
-
-          <label htmlFor="hunter-check">Include Hunters</label>
-          <div className="form-input">
-            <input type="checkbox" name="hunter-check" id="hunter-check"
-              checked={this.props.Hunter} onChange={this.toggleChangeGod("Hunter")} />
-          </div>
-
-          <label htmlFor="mage-check">Include Mages</label>
-          <div className="form-input">
-            <input type="checkbox" name="mage-check" id="mage-check"
-              checked={this.props.Mage} onChange={this.toggleChangeGod("Mage")} />
-          </div>
-
-          <label htmlFor="warrior-check">Include Warriors</label>
-          <div className="form-input">
-            <input type="checkbox" name="warrior-check" id="warrior-check"
-              checked={this.props.Warrior} onChange={this.toggleChangeGod("Warrior")} />
-          </div>
-
-          <button className="randomize-god-button" 
-          // onClick={this.toggleHidden.bind(this)}
-          >Random God!</button>
-        </form>
-        {/* {!this.state.isHidden &&  }*/}
-        <GodResultPage 
-        god={this.state.god} 
-        image={this.state.image} 
-        class={this.state.class}
-        // user={this.state.currentUser}
-        />
-        
+       {form}
+        <GodResultPage god={this.state.god} image={this.state.image} class={this.state.class}/>
       </div>
     );
   }
 }
-
 const mapStateToProps = state => ({...state.reducer, currentUser: state.auth.currentUser});
-
 export default connect(mapStateToProps)(RandomizeGodForm)
