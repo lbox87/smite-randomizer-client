efd91c6a7db08f278866df78a729c25259689d9c
Andrew Kolb <akolb87@gmail.com>
2019-04-29 20:25:45 -0500

    cleanup

--- src/store.js
+++ src/store.js
@@ -25,7 +25,6 @@ const store = createStore(
     applyMiddleware(thunk)
 );
 
-// Hydrate the authToken from localStorage if it exist
 const authToken = loadAuthToken();
 if (authToken) {
     const token = authToken;
