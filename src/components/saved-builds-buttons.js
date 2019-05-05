6fcd141b24e11be679e555cda79323f47dcea013
Andrew Kolb <akolb87@gmail.com>
2019-05-05 17:52:28 -0500

    prep for edit

--- src/components/saved-builds-buttons.js
+++ src/components/saved-builds-buttons.js
@@ -5,7 +5,56 @@ const { API_BASE_URL } = require('./config');
 export default class SavedBuildsButtons extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { deleteConfirmed: "" }
+        this.state = {
+            editGod: "",
+            editGodImage: "",
+            editItem1: "",
+            editItem2: "",
+            editItem3: "",
+            editItem4: "",
+            editItem5: "",
+            editItem6: "",
+            editImage1: "",
+            editImage2: "",
+            editImage3: "",
+            editImage4: "",
+            editImage5: "",
+            editImage6: ""
+        }
+    }
+
+    onClickEdit = (event) => {
+        event.preventDefault();
+        let buildEdit = {
+            build: this.props.id,
+        }
+        fetch(API_BASE_URL + 'find/' + this.props.id, {
+            method: "POST",
+            body: JSON.stringify(buildEdit),
+            headers: { "Content-Type": "application/json" },
+        })
+            .then(response => {
+                if (response.ok) {
+                    return response.json();
+                }
+            })
+            .then(response => {
+                this.setState({ editGod: response.build.god })
+                this.setState({ editGodImage: response.build.image })
+                this.setState({ editItem1: response.build.item1 })
+                this.setState({ editItem2: response.build.item2 })
+                this.setState({ editItem3: response.build.item3 })
+                this.setState({ editItem4: response.build.item4 })
+                this.setState({ editItem5: response.build.item5 })
+                this.setState({ editItem6: response.build.item6 })
+                this.setState({ editImage1: response.build.image1 })
+                this.setState({ editImage2: response.build.image2 })
+                this.setState({ editImage3: response.build.image3 })
+                this.setState({ editImage4: response.build.image4 })
+                this.setState({ editImage5: response.build.image5 })
+                this.setState({ editImage6: response.build.image6 })
+                console.log(this.state)
+            })
     }
 
     onClickDelete = (event) => {
@@ -18,18 +67,13 @@ export default class SavedBuildsButtons extends React.Component {
             body: JSON.stringify(buildDelete),
             headers: { "Content-Type": "application/json" },
         })
-            // .then(response => {
-            //     if (response.ok) {
-            //         return response.json();
-            //     }
-            // })
-            .then(  this.props.toggleClick  )
+            .then(this.props.toggleClick)
     }
 
     render() {
         return (
             <div className="god-button-options">
-                <button id={this.props.id} type="button" className="edit-build-button"> Edit Build </button>
+                <button id={this.props.id} type="button" className="edit-build-button" onClick={this.onClickEdit}> Edit Build </button>
                 <button id={this.props.id} type="button" className="delete-build-button" onClick={this.onClickDelete}> Delete Build </button>
             </div>
         );
