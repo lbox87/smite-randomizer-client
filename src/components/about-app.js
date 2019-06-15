027ac6971a8270ba97c33531d125062c9445c7e1
Andrew Kolb <akolb87@gmail.com>
2019-06-15 12:00:37 -0500

    landing update

--- src/components/about-app.js
+++ src/components/about-app.js
@@ -8,12 +8,14 @@ export default function AboutApp() {
             <p>
                 SMITE God Randomizer is a companion app to HiRez's video game "SMITE" that
                 allows you to randomize any God from the game's roster.
-                Once a God is randomed, you can also random the God's item build. If you create a profile,
+                Once a God is randomed, you can also randomize the God's item build. If you create a profile,
                 you will be able to save and revisit your favorite builds!
+                Sign in below or click "Register" at the top if you don't have an account. Click "Continue without log in" if you want to jump
+                right into randomizing. 
             </p>
             <p>
-                Sign in below or click "Register" at the top if you don't have an account. Click "Continue without log in" if you want to jump
-                right into randomizing.
+                To explore the full functionality of the app without registering, try out the test login: 
+                 <p>Username: test / Password: 1234567890</p>
             </p>
         </div>
     );
